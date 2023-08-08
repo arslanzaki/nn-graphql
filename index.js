12b050c33b5d9ca48e353465cf848173bbdc59c6
arslanzaki <arslanzaki223@gmail.com>
2023-08-08 11:02:11 +0500

    Resolver Functions

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+
+// Types
+import { typeDefs } from "./schema.js";
+
+// db
+import db from "./_db.js";
+
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 // Server Setup
 const server = new ApolloServer({
   // typeDefs -> short for type definitions. These are basically descriptions of our datatypes and the relationship they have with other datatypes
   typeDefs,
+  resolvers
   // resolvers -> a bunch of resolver functions that determine how we respond to queries for the different data on the graph
 });
 
